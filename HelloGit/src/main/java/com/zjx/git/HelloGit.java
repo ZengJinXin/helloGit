45bf1a8e9533da2f38374da09d06d4a623438e7a
ZengJinXin <727732501@qq.com>
2020-09-15 20:58:55 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/zjx/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.zjx.git;
+
+/**
+ * @author ZJX
+ * @create 2020- 09 -15 -20:48
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
