37c1b58b09d4748b90975b1fcdae0950966c91fb
ZengJinXin <727732501@qq.com>
2020-09-15 21:00:38 +0800

    更新2

--- HelloGit/src/main/java/com/zjx/git/HelloGit.java
+++ HelloGit/src/main/java/com/zjx/git/HelloGit.java
@@ -7,5 +7,7 @@ package com.zjx.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
